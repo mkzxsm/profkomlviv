e8f59d2a6ae27d4a00bfb52dda0614964c54cc51
romashka <maxym.martyn@gmail.com>
2026-04-17 21:35:41 +0300

    update gitignore

--- backend/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ backend/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfd46164bedddfcff1adfdccfbaa93260c0782e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41e5bcd153a963fadf9e5c79a3309b040da58b78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
